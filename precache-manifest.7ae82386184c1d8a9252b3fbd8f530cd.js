5a46386aeb43663675a8161317fb4b3a39a0679f
Bayon Forte <bayon@forteworks.com>
2018-11-17 08:19:01 -0500

    Updates

---
+++ precache-manifest.7ae82386184c1d8a9252b3fbd8f530cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66e01a8977a9d2cce54a",
+    "url": "/react-crud/static/js/runtime~main.66e01a89.js"
+  },
+  {
+    "revision": "bb4ef2a662f22016f10a",
+    "url": "/react-crud/static/js/main.bb4ef2a6.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/react-crud/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "bb4ef2a662f22016f10a",
+    "url": "/react-crud/static/css/main.84659a64.chunk.css"
+  },
+  {
+    "revision": "8df9da7358e7fc6bec2eed51cf02e5bd",
+    "url": "/react-crud/index.html"
+  }
+];
